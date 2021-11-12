f4594043fc402c0b5dc5e14d5da5bf921752b9f0
Rossello- Poulet Arthur <arthur.rossello-poulet@insa-lyon.fr>
2021-11-12 16:59:15 +0100

    :construction: Version fonctionnelle de la comparaison, à rendre propre

--- src/js/compare-clubs.js
+++ src/js/compare-clubs.js
@@ -26,6 +26,12 @@ document.addEventListener("DOMContentLoaded", () => {
         let classement_equipe1 = document.querySelector("#classementEquipe1");
         let object_found = data.results.bindings;
         let equipe1 = {};
+        // pour stocker les victoires et défaites
+        let victoires_larges = [];
+        let defaites_larges = [];
+        // pour eviter les doublons
+        let defaite_temporaire = [];
+        let victoire_temporaire = [];
         //equipe1["name"] = object_found[0]["dateDeNaissance"]["value"];
         equipe1["ranking"] = object_found[0]["classement"]["value"];
         classement_equipe1.innerHTML = equipe1["ranking"];
@@ -41,6 +47,113 @@ document.addEventListener("DOMContentLoaded", () => {
         buts_encaisses_1.innerHTML = equipe1["goalsAgainst"];
         equipe1["groundCapacity"] = object_found[0]["nombreDePlacesDansLeStade"]["value"];
         capacite_stade1.innerHTML = equipe1["groundCapacity"];
+        equipe1["name"] = object_found[0]["nom"]["value"];
+        nom_equipe1.innerHTML = equipe1["name"];
+
+        for (let o of object_found) {
+            console.log(victoire_temporaire)
+            if(o["plusLargeVictoire"]["value"].split("–").length>1 && !victoire_temporaire.includes(o["plusLargeVictoire"]["value"])) {
+                victoire_temporaire.push(o["plusLargeVictoire"]["value"])
+                let score = o["plusLargeVictoire"]["value"].split("–");
+                let espaceScore1 = score[0].split(" ");
+                let tailleScorePart1 = espaceScore1.length;
+                let number1 = espaceScore1[tailleScorePart1-1];
+                let i=0;
+                let newEspaceScore1 = [];
+                while(i<tailleScorePart1-1){
+                    newEspaceScore1[i] = espaceScore1[i];
+                    i++;
+                }
+                let espaceScore2 = score[1].split(" ");
+
+                let number2 = espaceScore2[0];
+                let tailleScorePart2 = espaceScore2.length;
+
+                let j=1;
+                let newEspaceScore2 = [];
+                while(j<tailleScorePart2){
+                    newEspaceScore2[j] = espaceScore2[j];
+                    j++;
+                }
+                let equipeDomicile = newEspaceScore1.join(" ");
+                let equipeExterieur = newEspaceScore2.join(" ");
+                let resultat = [];
+                resultat.push(equipeDomicile.trim());
+                resultat.push(number1.trim());
+                resultat.push(number2.trim());
+                resultat.push(equipeExterieur.trim());
+                victoires_larges.push(resultat);
+            }
+            if(o["plusLargeDefaite"]["value"].split("–").length>1 && !defaite_temporaire.includes(o["plusLargeDefaite"]["value"])) {
+                defaite_temporaire.push(o["plusLargeDefaite"]["value"])
+                let scoreD = o["plusLargeDefaite"]["value"].split("–");
+                let espaceScore1D = scoreD[0].split(" ");
+                let tailleScorePart1D = espaceScore1D.length;
+                let number1D = espaceScore1D[tailleScorePart1D-1];
+                let iD=0;
+                let newEspaceScore1D = [];
+                while(iD<tailleScorePart1D-1){
+                    newEspaceScore1D[iD] = espaceScore1D[iD];
+                    iD++;
+                }
+                let espaceScore2D = scoreD[1].split(" ");
+
+                let number2D = espaceScore2D[0];
+                let tailleScorePart2D = espaceScore2D.length;
+
+                let jD=1;
+                let newEspaceScore2D = [];
+                while(jD<tailleScorePart2D){
+                    newEspaceScore2D[jD] = espaceScore2D[jD];
+                    jD++;
+                }
+                let equipeDomicileD = newEspaceScore1D.join(" ");
+                let equipeExterieurD = newEspaceScore2D.join(" ");
+                let resultatD = [];
+                resultatD.push(equipeDomicileD.trim());
+                resultatD.push(number1D.trim());
+                resultatD.push(number2D.trim());
+                resultatD.push(equipeExterieurD.trim());
+                defaites_larges.push(resultatD);
+            }
+        }
+
+        // clone the nodes of the biggestWins
+        let biggestWin_node = document.getElementById("largeVictoire_result").lastElementChild;
+        for (let t of victoires_larges) {
+            let node = biggestWin_node.cloneNode(true);
+            let div1 = node.querySelector(".team-score.team1");
+            let p1 = div1.querySelector("p");
+            p1.innerHTML = t[0];
+            let div2 = node.querySelector(".element-content.score");
+            let p2 = div2.querySelector("p");
+            p2.innerHTML = t[1]+"-"+t[2];
+            let div3 = node.querySelector(".team-score.team2");
+            let p3 = div3.querySelector("p");
+            p3.innerHTML = t[3];
+            document.getElementById("largeVictoire_result").appendChild(node);
+        }
+        biggestWin_node.remove();
+
+        let biggestLose_node = document.getElementById("largeDefaite_result").lastElementChild;
+        for (let t of defaites_larges) {
+            let node = biggestLose_node.cloneNode(true);
+            let div1 = node.querySelector(".team-score.team1");
+            let p1 = div1.querySelector("p");
+            p1.innerHTML = t[0];
+            let div2 = node.querySelector(".element-content.score");
+            let p2 = div2.querySelector("p");
+            p2.innerHTML = t[1]+"-"+t[2];
+            let div3 = node.querySelector(".team-score.team2");
+            let p3 = div3.querySelector("p");
+            p3.innerHTML = t[3];
+            document.getElementById("largeDefaite_result").appendChild(node);
+        }
+        biggestLose_node.remove();
+
+
+
+
         hideSpinner();
         showContent();
     });
@@ -57,6 +170,10 @@ document.addEventListener("DOMContentLoaded", () => {
         let classement_equipe2 = document.querySelector("#classementEquipe2");
         let object_found = data.results.bindings;
         let equipe2 = {};
+        let victoires_larges2 = [];
+        let defaites_larges2 = [];
+        let defaite_temporaire2 = [];
+        let victoire_temporaire2 = [];
         //equipe2["name"] = object_found[0]["dateDeNaissance"]["value"];
         equipe2["ranking"] = object_found[0]["classement"]["value"];
         classement_equipe2.innerHTML = equipe2["ranking"];
@@ -72,6 +189,111 @@ document.addEventListener("DOMContentLoaded", () => {
         buts_encaisses_2.innerHTML = equipe2["goalsAgainst"];
         equipe2["groundCapacity"] = object_found[0]["nombreDePlacesDansLeStade"]["value"];
         capacite_stade2.innerHTML = equipe2["groundCapacity"];
+        equipe2["name"] = object_found[0]["nom"]["value"];
+        nom_equipe2.innerHTML = equipe2["name"];
+
+
+        for (let o of object_found) {
+            if(o["plusLargeVictoire"]["value"].split("–").length>1 && !victoire_temporaire2.includes(o["plusLargeVictoire"]["value"])) {
+                victoire_temporaire2.push(o["plusLargeVictoire"]["value"])
+                let score = o["plusLargeVictoire"]["value"].split("–");
+                let espaceScore1 = score[0].split(" ");
+                let tailleScorePart1 = espaceScore1.length;
+                let number1 = espaceScore1[tailleScorePart1-1];
+                let i=0;
+                let newEspaceScore1 = [];
+                while(i<tailleScorePart1-1){
+                    newEspaceScore1[i] = espaceScore1[i];
+                    i++;
+                }
+                let espaceScore2 = score[1].split(" ");
+
+                let number2 = espaceScore2[0];
+                let tailleScorePart2 = espaceScore2.length;
+
+                let j=1;
+                let newEspaceScore2 = [];
+                while(j<tailleScorePart2){
+                    newEspaceScore2[j] = espaceScore2[j];
+                    j++;
+                }
+                let equipeDomicile = newEspaceScore1.join(" ");
+                let equipeExterieur = newEspaceScore2.join(" ");
+                let resultat = [];
+                resultat.push(equipeDomicile.trim());
+                resultat.push(number1.trim());
+                resultat.push(number2.trim());
+                resultat.push(equipeExterieur.trim());
+                victoires_larges2.push(resultat);
+            }
+            if(o["plusLargeDefaite"]["value"].split("–").length>1 && !defaite_temporaire2.includes(o["plusLargeDefaite"]["value"])) {
+                defaite_temporaire2.push(o["plusLargeDefaite"]["value"])
+                let scoreD = o["plusLargeDefaite"]["value"].split("–");
+                let espaceScore1D = scoreD[0].split(" ");
+                let tailleScorePart1D = espaceScore1D.length;
+                let number1D = espaceScore1D[tailleScorePart1D-1];
+                let iD=0;
+                let newEspaceScore1D = [];
+                while(iD<tailleScorePart1D-1){
+                    newEspaceScore1D[iD] = espaceScore1D[iD];
+                    iD++;
+                }
+                let espaceScore2D = scoreD[1].split(" ");
+
+                let number2D = espaceScore2D[0];
+                let tailleScorePart2D = espaceScore2D.length;
+
+                let jD=1;
+                let newEspaceScore2D = [];
+                while(jD<tailleScorePart2D){
+                    newEspaceScore2D[jD] = espaceScore2D[jD];
+                    jD++;
+                }
+                let equipeDomicileD = newEspaceScore1D.join(" ");
+                let equipeExterieurD = newEspaceScore2D.join(" ");
+                let resultatD = [];
+                resultatD.push(equipeDomicileD.trim());
+                resultatD.push(number1D.trim());
+                resultatD.push(number2D.trim());
+                resultatD.push(equipeExterieurD.trim());
+                defaites_larges2.push(resultatD);
+            }
+        }
+
+        console.log(victoires_larges2)
+
+        // clone the nodes of the biggestWins
+        let biggestWin_node = document.getElementById("largeVictoire_result2").lastElementChild;
+        for (let t of victoires_larges2) {
+            let node = biggestWin_node.cloneNode(true);
+            let div1 = node.querySelector(".team-score.team1");
+            let p1 = div1.querySelector("p");
+            p1.innerHTML = t[0];
+            let div2 = node.querySelector(".element-content.score");
+            let p2 = div2.querySelector("p");
+            p2.innerHTML = t[1]+"-"+t[2];
+            let div3 = node.querySelector(".team-score.team2");
+            let p3 = div3.querySelector("p");
+            p3.innerHTML = t[3];
+            document.getElementById("largeVictoire_result2").appendChild(node);
+        }
+        biggestWin_node.remove();
+
+        let biggestLose_node = document.getElementById("largeDefaite_result2").lastElementChild;
+        for (let t of defaites_larges2) {
+            let node = biggestLose_node.cloneNode(true);
+            let div1 = node.querySelector(".team-score.team1");
+            let p1 = div1.querySelector("p");
+            p1.innerHTML = t[0];
+            let div2 = node.querySelector(".element-content.score");
+            let p2 = div2.querySelector("p");
+            p2.innerHTML = t[1]+"-"+t[2];
+            let div3 = node.querySelector(".team-score.team2");
+            let p3 = div3.querySelector("p");
+            p3.innerHTML = t[3];
+            document.getElementById("largeDefaite_result2").appendChild(node);
+        }
+        biggestLose_node.remove();
         hideSpinner();
         showContent();
     });
@@ -87,3 +309,5 @@ function hideSpinner() {
     document.getElementById('spinner').remove();
     document.getElementById('middleSpinner').remove();
 }
+
+
